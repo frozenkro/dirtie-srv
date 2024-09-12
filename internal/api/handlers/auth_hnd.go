8b9b699dfc4cc330334aac5df33ad6fa6c4a0d45
kaup <kalebroyolson+github@gmail.com>
2024-09-11 23:02:59 -0500

    login/logout handlers

--- internal/api/handlers/auth_hnd.go
+++ internal/api/handlers/auth_hnd.go
@@ -15,11 +15,15 @@ type CreateUserArgs struct {
   name string
 }
 
+type LoginArgs struct {
+  email string
+  password string
+}
+
 func SetupAuthHandlers(deps *core.Deps) {
   http.Handle("POST /users", middleware.Adapt(
     createUserHandler(deps.AuthSvc),
     middleware.LogTransaction(),
-    middleware.Authorize(deps.AuthSvc),
   ))
 }
 
@@ -46,6 +50,40 @@ func createUserHandler(authSvc services.AuthSvc) http.Handler {
   })
 }
 
-func forgotPasswordHandler() {}
+func loginHandler(authSvc services.AuthSvc) http.Handler {
+  return http.HandlerFunc(func (w http.ResponseWriter, r  *http.Request) {
+    var args LoginArgs
 
-func resetPasswordHandler() {}
+    err := json.NewDecoder(r.Body).Decode(&args)
+    if err != nil {
+      http.Error(w, core.RequestParseError, http.StatusBadRequest)
+    }
+
+    token, err := authSvc.Login(r.Context(), args.email, args.password)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    
+    cookie := http.Cookie {
+      Name: "dirtie.auth",
+      Value: token,
+    }
+    http.SetCookie(w, &cookie)
+  })
+}
+
+func logoutHandler(authSvc services.AuthSvc) http.Handler {
+  return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+    cookie, err := r.Cookie("dirtie.auth")
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusBadRequest)
+    }
+
+    err = authSvc.Logout(r.Context(), cookie.Value)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
+    w.WriteHeader(http.StatusOK)
+  })
+}
