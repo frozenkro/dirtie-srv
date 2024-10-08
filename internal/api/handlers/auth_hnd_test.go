0af0e93bcd2af33be325761d268a9f13caaa985d
Kaleb Olson <kalebroyolson+github@gmail.com>
2024-10-08 17:34:59 -0500

    refactor integration tests to use test runner

--- internal/api/handlers/auth_hnd_test.go
+++ internal/api/handlers/auth_hnd_test.go
@@ -12,85 +12,76 @@ import (
 	"github.com/frozenkro/dirtie-srv/internal/core/int_tst"
 	"github.com/frozenkro/dirtie-srv/internal/db/sqlc"
 	"github.com/frozenkro/dirtie-srv/internal/di"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateUser_CreatesUser(t *testing.T) {
+func TestCreateUser(t *testing.T) {
   core.SetupTestEnv()
   int_tst.SetupTests()
-  deps := di.NewDeps()
-  server := httptest.NewServer(createUserHandler(deps.AuthSvc))
-  defer server.Close()
-
-  userArgs := CreateUserArgs{
-    Email: "createusertest@email.com",
-    Password: "createuserpassword",
-    Name: "Test User",
-  }
-  userBytes, err := json.Marshal(userArgs)
-  if err != nil {
-    t.Fatalf("Error encoding request body: %v", err)
-  }
-
-  resp, err := http.Post(server.URL+"/users", "application/json", bytes.NewBuffer(userBytes))
-  if err != nil {
-    t.Errorf("API client returned error: %v", err)
-  }
-  defer resp.Body.Close()
-
-  if resp.StatusCode != http.StatusOK {
-    t.Errorf("API status code unexpected: %v", resp.StatusCode)
-  }
-  
   db := int_tst.ConnectDb()
-  row, err := db.Query(
-    context.Background(),
-    "SELECT * FROM users WHERE email = $1", 
-    userArgs.Email,
-  )
-
-  if !row.Next() {
-    t.Errorf("No rows found")
-  }
-
-  user := sqlc.User{}
-  err = row.Scan(&user.UserID, &user.Email, &user.Name, &user.PwHash, &user.CreatedAt, &user.LastLogin)
-  if err != nil {
-    t.Fatalf("Error converting inserted row to user obj: %v", err)
-  }
-
-  if row.Next() {
-    t.Errorf("Multiple rows found")
-  }
-
-  if user.Name != userArgs.Name {
-    t.Errorf("Inserted user Name mismatch")
-  }
-}
-
-func TestCreateUser_WhenUserEmailExists_ReturnsUserExistsError(t *testing.T) {
-  core.SetupTestEnv()
-  int_tst.SetupTests()
   deps := di.NewDeps()
   server := httptest.NewServer(createUserHandler(deps.AuthSvc))
   defer server.Close()
 
-  userArgs := CreateUserArgs{
-    Email: int_tst.TestUser.Email,
-    Password: "createuserpassword",
-    Name: "Test User",
-  }
-  userBytes, err := json.Marshal(userArgs)
-  if err != nil {
-    t.Fatalf("Error encoding request body: %v", err)
-  }
+  t.Run("Success", func(t *testing.T) {
 
-  resp, err := http.Post(server.URL+"/users", "application/json", bytes.NewBuffer(userBytes))
-  if err != nil {
-    t.Errorf("API client returned error: %v", err)
-  }
-  defer resp.Body.Close()
+    userArgs := CreateUserArgs{
+      Email: "createusertest@email.com",
+      Password: "createuserpassword",
+      Name: "Test User",
+    }
+    userBytes, err := json.Marshal(userArgs)
+    if err != nil {
+      t.Fatalf("Error encoding request body: %v", err)
+    }
 
-  if resp.StatusCode != http.StatusInternalServerError {
-    t.Errorf("API status code unexpected: %v", resp.StatusCode)
-  }
+    resp, err := http.Post(server.URL+"/users", "application/json", bytes.NewBuffer(userBytes))
+    if err != nil {
+      t.Errorf("API client returned error: %v", err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+      t.Errorf("API status code unexpected: %v", resp.StatusCode)
+    }
+    
+    row, err := db.Query(
+      context.Background(),
+      "SELECT * FROM users WHERE email = $1", 
+      userArgs.Email,
+    )
+
+    if !row.Next() {
+      t.Errorf("No rows found")
+    }
+
+    user := sqlc.User{}
+    err = row.Scan(&user.UserID, &user.Email, &user.Name, &user.PwHash, &user.CreatedAt, &user.LastLogin)
+    if err != nil {
+      t.Fatalf("Error converting inserted row to user obj: %v", err)
+    }
+
+    assert.False(t, row.Next())
+    assert.Equal(t, user.Name, userArgs.Name)
+  })
+
+  t.Run("UserAlreadyExists", func(t *testing.T) {
+    userArgs := CreateUserArgs{
+      Email: int_tst.TestUser.Email,
+      Password: "createuserpassword",
+      Name: "Test User",
+    }
+    userBytes, err := json.Marshal(userArgs)
+    if err != nil {
+      t.Fatalf("Error encoding request body: %v", err)
+    }
+
+    resp, err := http.Post(server.URL+"/users", "application/json", bytes.NewBuffer(userBytes))
+    if err != nil {
+      t.Errorf("API client returned error: %v", err)
+    }
+    defer resp.Body.Close()
+
+    assert.Equal(t, resp.StatusCode, http.StatusInternalServerError)
+  })
 }
