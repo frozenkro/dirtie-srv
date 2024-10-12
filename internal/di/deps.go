c0e72e5980f5a72e295bef8650cae34b17dd0ace
kaup <kalebroyolson+github@gmail.com>
2024-10-12 00:05:41 -0500

    mqtt topic routing and DI

--- internal/di/deps.go
+++ internal/di/deps.go
@@ -4,10 +4,15 @@ import (
 	"github.com/frozenkro/dirtie-srv/internal/core/utils"
 	"github.com/frozenkro/dirtie-srv/internal/db"
 	"github.com/frozenkro/dirtie-srv/internal/db/repos"
+	brdcrm_topic "github.com/frozenkro/dirtie-srv/internal/hub/topics/brdcrmtopic"
+	prv_topic "github.com/frozenkro/dirtie-srv/internal/hub/topics/prvtopic"
 	"github.com/frozenkro/dirtie-srv/internal/services"
 )
 
 type Deps struct {
+  BrdCrmTopic    *brdcrm_topic.BrdCrmTopic
+  ProvisionTopic *prv_topic.ProvisionTopic
+
 	AuthSvc   services.AuthSvc
 	BrdCrmSvc services.BrdCrmSvc
 	DeviceSvc services.DeviceSvc
@@ -48,7 +53,12 @@ func NewDeps() *Deps {
 	deviceSvc := services.NewDeviceSvc(deviceRepo, provStgRepo, ctxUtil)
 	brdCrmSvc := services.NewBrdCrmSvc(influxRepo, influxRepo, deviceSvc)
 
+  brdCrmTopic := brdcrm_topic.NewBrdCrmTopic(brdCrmSvc)
+  prvTopic := prv_topic.NewProvisionTopic(*deviceSvc)
+
 	return &Deps{
+    BrdCrmTopic:         brdCrmTopic,
+    ProvisionTopic:      prvTopic,
 		AuthSvc:             *authSvc,
 		BrdCrmSvc:           brdCrmSvc,
 		DeviceSvc:           *deviceSvc,
--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -1,47 +1,59 @@
 package hub
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/eclipse/paho.mqtt.golang"
 	"github.com/frozenkro/dirtie-srv/internal/core/topics"
+	"github.com/frozenkro/dirtie-srv/internal/core/utils"
+	"github.com/frozenkro/dirtie-srv/internal/di"
 )
 
 var (
 	totalReconnectAttempts int = 10
 	client                 mqtt.Client
+  deps                   *di.Deps
 )
 
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	topic := string(msg.Topic())
-	fmt.Printf("Received message: %s from topic %s\n", string(msg.Payload()), topic)
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
 
+	topic := string(msg.Topic())
+	utils.LogInfo(fmt.Sprintf("Received message: %s from topic %s\n", string(msg.Payload()), topic))
+
+  var err error
 	switch topic {
-	case topics.Breadcrumb:
-		// need deps injected
-	case topics.Provision:
-		// yep still need deps
-	default:
-		fmt.Printf("Topic %v not recognized", topic)
+    case topics.Breadcrumb:
+      err = deps.BrdCrmTopic.InvokeTopic(ctx, msg.Payload())
+    case topics.Provision:
+      err = deps.ProvisionTopic.InvokeTopic(ctx, msg.Payload())
+    default:
+      utils.LogInfo(fmt.Sprintf("Topic %v not recognized", topic))
 	}
+
+  if err != nil {
+    utils.LogErr(fmt.Errorf("Error MessagePubHandler -> InvokeTopic: %w", err).Error())
+  }
 }
 
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-	fmt.Printf("connected to mqtt broker\n")
+	utils.LogInfo(fmt.Sprintf("connected to mqtt broker\n"))
 }
 
 var connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	fmt.Printf("disconnected from mqtt broker\n")
+	utils.LogInfo(fmt.Sprintf("disconnected from mqtt broker\n"))
 	attemptReconnect()
 }
 
 func attemptReconnect() {
 	for i := 1; i <= totalReconnectAttempts; i++ {
-		fmt.Printf("attempting to reconnect (%d/%d)\n", i, totalReconnectAttempts)
+		utils.LogInfo(fmt.Sprintf("attempting to reconnect (%d/%d)\n", i, totalReconnectAttempts))
 
 		if token := client.Connect(); token.Wait() && token.Error() == nil {
-			fmt.Printf("Reconnect successful\n")
+			utils.LogInfo(fmt.Sprintf("Reconnect successful\n"))
 			return
 		}
 	}
@@ -53,6 +65,8 @@ func Init() {
 	if !ok {
 		uri = "localhost:1883"
 	}
+
+  deps = di.NewDeps()
 
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(fmt.Sprintf("tcp://%s", uri))
